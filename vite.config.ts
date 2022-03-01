20dff96f57590074a31349624f5669c320e3f8c4
Vyacheslav <ermak519@gmail.com>
2022-03-01 23:00:00 +0300

    start dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteFonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(), 
+    ViteFonts({
+    google: {
+      families: [
+        {
+          name: 'Roboto',
+          styles: 'ital,wght@0,400;1,200',
+          defer: true
+        },
+        {
+          name: 'Inter',
+          styles: 'ital,wght@0,400;1,200',
+          defer: true
+        },
+    ],
+    },
+  })]
 })
